f9e33ddad4ebde8f35b11ea6bddc34b4d208f17b
Peter Munene <68741159+munenepeter@users.noreply.github.com>
2022-03-04 19:55:14 +0300

    updated with local

--- index.php
+++ index.php
@@ -4,155 +4,167 @@
 <head>
     <meta charset="UTF-8">
     <meta name="title" content="P-translate">
-    <meta name="description" content="A Simple way of parsing pdf, docx, text or rtf files so as to identify certain keywords using pearl">
+    <meta name="description"
+        content="A Simple way of parsing pdf, docx, text or rtf files so as to identify certain keywords using pearl">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="robots" content="index, follow">
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-    <meta name="author" content="Peter Munene, Abzed Mohamed">
-    <!-- Font Awesome -->
+    <meta name="author" content="Peter Munene, Abzed Mohamed Maxwell Kanuro">
+    <link rel="shortcut icon" href="static/imgs/translate.svg" type="image/svg">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" rel="stylesheet" />
-    <!-- Google Fonts -->
-    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" rel="stylesheet" />
-    <!-- MDB -->
     <link href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/3.10.2/mdb.min.css" rel="stylesheet" />
-
-
-    <link rel="stylesheet" href="src/style.css">
+    <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
+    <link rel="stylesheet" href="static/css/style.css" media="(prefers-color-scheme: dark)">
     <title>P-translate</title>
 </head>
 
 <body>
-    <?php
 
-    // include composer autoloader
-    include 'vendor/autoload.php';
-    include 'src/DocumentParser.php';
-    include 'src/functions.php';
+    <div class="container pt-8">
+        <div class="card">
 
-    // $logger->log('Successfully included'. json_encode(get_included_files()));
-    ?>
-    <div class="container">
+            <!-- Tabs navs -->
 
-        <div class="card text-center">
-            <div class="card-header">
-                <ul class="nav nav-tabs card-header-tabs">
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="true" href="#!">Convert PDF</a>
-                    </li>
-
-                    <li class="nav-item">
-                        <a class="nav-link disabled" href="#!">Text Translate</a>
-                    </li>
-                </ul>
-            </div>
-
-            <div class="card-body">
-                <div id="translate">
-                    <h5 class="card-title">Welcome to Document Translator</h5>
-                    <p class="card-text">Select document to translate</p>
-                    <br>
-                    <form action="" method="post" enctype="multipart/form-data">
-                        <div class='file-input'>
-                            <input type='file' name="file" accept=".xlsx,.xls,.doc, .docx,.ppt, .pptx,.txt,.pdf">
-                            <span class='button'>Choose</span>
-                            <span class='label' data-js-label>No file selected</label>
-                        </div>
-                        <br>
-                        <button name="submitpdf" type="submit" id="submitpdf" class="btn btn-primary">Upload</button>
-                    </form>
+            <ul class="nav nav-tabs mb-3" id="ex1" role="tablist">
+                <li class="nav-item" role="presentation">
+                    <a class="nav-link active" id="ex1-tab-1" data-mdb-toggle="tab" href="#ex1-tabs-1" role="tab"
+                        aria-controls="ex1-tabs-1" aria-selected="true">Convert Document</a>
+                </li>
+                <li class="nav-item" role="presentation">
+                    <a class="nav-link" id="ex1-tab-2" data-mdb-toggle="tab" href="#ex1-tabs-2" role="tab"
+                        aria-controls="ex1-tabs-2" aria-selected="false">Text Translate</a>
+                </li>
+                <li class="nav-item" role="presentation">
+                    <a class="nav-link" id="ex1-tab-3" data-mdb-toggle="tab" href="#ex1-tabs-3" role="tab"
+                        aria-controls="ex1-tabs-3" aria-selected="false">URL Translate</a>
+                </li>
+                <div class="nav-item mode">
+                    <input type="checkbox" class="checkbox" id="checkbox">
+                    <label for="checkbox" class="label">
+                        <i class="fas fa-moon"></i>
+                        <i class='fas fa-sun'></i>
+                        <div class='ball'>
+                    </label>
                 </div>
-                <div id="details">
+            </ul>
+            <hr>
+            <!-- Tabs navs -->
 
-                    <?php
+            <!-- Tabs content -->
+            <div class="tab-content" id="ex1-content">
+                <div class="tab-pane fade show active" id="ex1-tabs-1" role="tabpanel" aria-labelledby="ex1-tab-1">
+                    <div class="card-body">
+                        <div id="translate">
+                            <h5 class="card-title">Welcome to Document Translator</h5>
+                            <i class="fab fa-cloud-upload"></i>
+                            <br>
+                            <form method="post" enctype="multipart/form-data">
+                                <p>Select document to translate</p>
+                                <div class='file-input'>
+                                    <input type='file' name="files"
+                                        accept=".xlsx,.xls,.doc, .docx,.ppt, .pptx,.txt,.pdf">
+                                    <span class='button'>Choose</span>
+                                    <span class='label' data-js-label>No file selected</label>
+                                </div>
+                                <br>
+                                <button name="submitpdf" type="submit" id="submitpdf"
+                                    class="btn btn-primary">Upload</button>
+                            </form>
+                            <div class="p-4 card text-left">
+                                <p id="content" style="text-align: left;"></p>
+                                <button id="clear" class="btn btn-primary" style="display:none;">Clear</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="tab-pane fade" id="ex1-tabs-2" role="tabpanel" aria-labelledby="ex1-tab-2">
+                    <div class="card-body">
+                        <div id="translate">
+                            <h5 class="card-title">Welcome to Text Translator</h5>
+                            <br>
+                            <form method="post" enctype="multipart/form-data">
+                                <div class="form-outline">
+                                    <textarea class="form-control" id="textAreaExample" rows="4"></textarea>
+                                    <label class="form-label" for="textAreaExample">Paste your content here</label>
+                                </div>
+                                <br>
+                                <button name="submitpdf" type="submit" id="submitpdf"
+                                    class="btn btn-primary">Upload</button>
+                            </form>
+                            <div class="p-4 card text-left">
+                                <p id="content" style="text-align: left;"></p>
+                                <button id="clear" class="btn btn-primary" style="display:none;">Clear</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="tab-pane fade" id="ex1-tabs-3" role="tabpanel" aria-labelledby="ex1-tab-3">
+                    <div class="card-body">
+                        <div id="translate">
+                            <h5 class="card-title">Welcome to URL Document</h5>
+                            <br>
+                            <form method="post" enctype="multipart/form-data">
 
-                    if (isset($_POST['submitpdf'])) {
-                        $start_time = microtime(true);
-                        // $logger->log('Submit was pressed and posted'. json_encode($_POST));
-                        $logger->log('Succesfully uploaded ' . json_encode($_FILES));
-
-                        // parse pdf
-
-                        if ($_FILES['file']['type'] === "application/pdf") {
-
-                            $parser = new \Smalot\PdfParser\Parser();
-                            try {
-                                $logger->log('Succesfully instantiated the Parser' . get_class($parser));
-
-                                $pdf = $parser->parseFile($_FILES['file']['tmp_name']);
-                                $text = $pdf->getText();
-
-                                $logger->log('Trying to parse ' . implode(', ', $pdf->getDetails()));
-
-                                $end_time = microtime(true);
-                                $execution_time = ($end_time - $start_time);
-                            } catch (\Exception $e) {
-
-                                echo '<div class="card" style="color:red;bg-color:red;"> <p class="p-4">' . $e->getMessage() .  '</p> </div> </div>';
-                                $logger->log('Error: An exception was called ' . $e->getMessage());
-
-                                return;
-                            }
-                        } else {
-                            try {
-                                $text = LukeMadhanga\DocumentParser::parseFromFile($_FILES['file']['tmp_name']);
-                            } catch (\Exception $e) {
-
-                                echo '<div class="card" style="color:red;bg-color:red;"> <p class="p-4">' . $e->getMessage() .  '</p> </div> </div>';
-                                $logger->log('Error: An exception was called ' . $e->getMessage());
-
-                                return;
-                            }
-                        }
-                    }
-                    ?>
+                                <div class="form-outline">
+                                    <input type="url" id="typeURL" class="form-control" />
+                                    <label class="form-label" for="typeURL">URL input</label>
+                                </div>
+                                <div id="textExample1" class="form-text">
+                                    Here please paste the url to the document.
+                                </div>
+                                <br>
+                                <button name="submitpdf" type="submit" id="submitpdf"
+                                    class="btn btn-primary">Upload</button>
+                            </form>
+                            <div class="p-4 card text-left">
+                                <p id="content" style="text-align: left;"></p>
+                                <button id="clear" class="btn btn-primary" style="display:none;">Clear</button>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
 
-        <div class="p-4 card">
-            <p class="
-                <?php
-                    if ($_FILES['file']['type'] === 'application/pdf') {
-                        echo 'p-4';
-                    } else {
-                        echo '';
+    </div>
+    <br>
+    <div class="container">
+        <div class="card">
+            <h5 class="card-header">Possible LI's</h5>
+            <div class="card-body">
+                <h5 class="card-title">The following have been found to be possible LI's within the file</h5>
+                <p class="card-text">Confirm and copy them to your clipboard</p>
+
+                <script>
+                    var Lis = document.getElementsByClassName("pearl-hilighted-word").value;
+                    // Lis = document.querySelectorAll('.pearl-highlighted-word');
+                    //  console.log(Lis);
+                </script>
+                <script>
+                    var closebtns = document.getElementsByClassName("close");
+                    var i;
+
+                    /* Loop through the elements, and hide the parent, when clicked on */
+                    for (i = 0; i < closebtns.length; i++) {
+                        closebtns[i].addEventListener("click", function () {
+                            this.parentElement.style.display = 'none';
+                        });
                     }
-                    ?>
-                ">
-                <?php
-
-                if (!empty($text)) {
-                    //$logger->log('Successfully parsed  '. reset($pdf->getDetails()) .' with  '. end($pdf->getDetails()).' pages in '. round($execution_time, 2). 'secs');
-                        echo $text;
-                    } else {
-                        // $logger->log('Error: Something has happened cause the file with  '. end($pdf->getDetails()).' pages has not been parsed ');
-                    }
-
-
-                clearstatcache();
-                ?>
-            </p>
+                </script>
+                <a href="#" onclick="copyText()" class="btn btn-primary">Copy LI's</a>
+            </div>
         </div>
     </div>
 
-    <footer style="margin-top: 220px;">
+    <footer style="margin-top: 15px;">
         <div class="container">
             <div class="row">
-                <script>
-                    fetch("https://type.fit/api/quotes")
-                        .then(function(response) {
-                            return response.json();
-                        })
-                        .then(function(data) {
-                            console.log(data);
-                        });
-                </script>
                 <div class="row text-center">
                     <div class="col-md-4 box">
                         <span class="copyright quick-links">Copyright &copy; P-translate
                             <script>
-                                document.write(new Date().getFullYear())
+                                document.writeln(new Date().getFullYear())
                             </script>
                         </span>
                     </div>
@@ -183,13 +195,29 @@
                             <li class="list-inline-item">
                                 <a href="https://github.com/munenepeter">Peter</a>
                             </li>
+                            <li class="list-inline-item">
+                                <a href="https://github.com/kejereme">Max</a>
+                            </li>
                         </ul>
                     </div>
                 </div>
             </div>
     </footer>
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/3.10.2/mdb.min.js"></script>
-    <script src="src/index.js"></script>
+    <script src="static/js/index.js"></script>
+    <script>
+        var myVar;
+
+        function myFunction() {
+            myVar = setTimeout(showPage, 3000);
+        }
+
+        function showPage() {
+            document.getElementById("loader").style.display = "none";
+            document.getElementById("myDiv").style.display = "block";
+        }
+    </script>
+
 </body>
 
 </html>
---
+++ static/js/index.js
@@ -0,0 +1,62 @@
+// Also see: https://www.quirksmode.org/dom/inputfile.html
+// document.addEventListener('load', ()=>{
+//   document.getElementById("translate").style.display = "none";
+// });
+
+
+const checkbox = document.getElementById('checkbox');
+
+checkbox.addEventListener('change', () => {
+  document.body.classList.toggle('dark');
+  localStorage.setItem("KEY", "value");
+  // localStorage.getItem("KEY");
+})
+
+// localStorage.setItem("Max", "Keme");
+var inputs = document.querySelectorAll('.file-input')
+
+for (var i = 0, len = inputs.length; i < len; i++) {
+  customInput(inputs[i])
+}
+
+function customInput(el) {
+  const fileInput = el.querySelector('[type="file"]')
+  const label = el.querySelector('[data-js-label]')
+
+  fileInput.onchange =
+    fileInput.onmouseout = function () {
+      if (!fileInput.value) return
+
+      var value = fileInput.value.replace(/^.*[\\\/]/, '')
+      el.className += ' -chosen'
+      label.innerText = value
+    }
+}
+
+const form = document.querySelector("form");
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const formData = new FormData(form);
+  const url = window.location.href + "/src/functions.php";
+  axios
+    .post(url, formData, {
+      headers: {
+        "Content-Type": "multipart/form-data",
+        
+      },
+    })
+    .then((res) => {
+      console.log(res);
+      document.getElementById('content').innerHTML = res.data;
+      document.getElementById("clear").style.display = "block";
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+const clearBtn = document.getElementById('clear');
+clearBtn.addEventListener('click', () => {
+  document.getElementById('content').innerHTML = '';
+  document.getElementById("clear").style.display = "none";
+});
