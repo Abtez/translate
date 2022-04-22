b67cea21377136c92f06aecac23c58008715223b
Peter Munene <68741159+munenepeter@users.noreply.github.com>
2022-04-22 19:31:21 +0300

    Added from local

---
+++ src/functions.php
@@ -0,0 +1,143 @@
+<?php
+
+use Smalot\PdfParser\Parser;
+use LukeMadhanga\DocumentParser;
+
+
+header('Content-Type: application/json; charset=utf-8');
+
+header("Access-Control-Allow-Origin: *");
+
+header("Access-Control-Allow-Methods: PUT, GET, POST");
+
+
+include '../vendor/autoload.php';
+
+$messages = [];
+
+$logger = new class {
+
+    public String $log = "";
+
+    public function log(String $message) {
+
+        $this->log = date("D, d M Y H:i:s") . ' - ' . $_SERVER['SERVER_NAME'] . ' - ' . $_SERVER['REMOTE_ADDR'] . ' - ' . "$message" . PHP_EOL;
+
+        $logFile =  "../Logs/log.log";
+
+        $file = fopen($logFile, 'a');
+        fwrite($file, $this->log);
+        fclose($file);
+    }
+};
+
+
+if ($_FILES['files']) {
+
+    $start_time = microtime(true);
+    $logger->log('INFO: Succesfully uploaded ' . json_encode($_FILES));
+
+    if ($_FILES['files']['type'] === "application/pdf") {
+
+        $parser = new Parser();
+        try {
+
+            $pdf = $parser->parseFile($_FILES['files']['tmp_name']);
+
+            $text = $pdf->getText();
+
+            $logger->log('INFO: Trying to parse ' . implode(', ', $pdf->getDetails()));
+
+            $end_time = microtime(true);
+            $execution_time = round(($end_time - $start_time), 4);
+        } catch (\Exception $e) {
+
+            $messages['error'] = '<div class="card" style="color:red;bg-color:red;"> <p class="p-4">' . $e->getMessage() .  '</p> </div> </div>';
+            $logger->log('ERROR: An exception was called ' . $e->getMessage());
+
+            return;
+        }
+    } else {
+        try {
+            $text = DocumentParser::parseFromFile($_FILES['files']['tmp_name']);
+        } catch (\Exception $e) {
+
+            $messages['error'] =  '<div class="card" style="color:red;bg-color:red;"> <p class="p-4">' . $e->getMessage() .  '</p> </div> </div>';
+            $logger->log('ERROR: An exception was called ' . $e->getMessage());
+
+            return;
+        }
+    }
+    if (!empty($text)) {
+        $logger->log("INFO: Successfully parsed " . $_FILES['files']['name'] . " in  $execution_time seconds!");
+        $messages['text'] = nl2br($text);
+    } else {
+        $logger->log('ERROR: Something has happened!');
+        $messages['error'] = "ERROR: Something has happened";
+    }
+
+
+  
+}
+
+$lis_found = [];
+
+$githubLis = json_decode(file_get_contents("https://munenepeter.github.io/my-file-tracker/data/datas.json"));
+
+$fileText = trim($text);
+
+ 
+ 
+$found = false;
+
+foreach ($githubLis as $githubLi) {
+    if (strpos($fileText, $githubLi->name) !== false) {
+        $found = true;
+        $lis_found[] = $githubLi->name;
+    }
+    $abbrs = explode(',', trim($githubLi->abbr));
+
+    foreach ($abbrs as $abbr) {
+        if (is_array($abbr)) {
+            foreach ($abbr as $sub_abbr) {
+                if (strpos($fileText, $sub_abbr) !== false) {
+                    $found = true;
+                    $lis_found[] = $githubLi->name;
+                }
+            }
+        }
+
+        if (strpos($fileText, $abbr) !== false) {
+            $found = true;
+            $lis_found[] = $githubLi->name;
+        }
+    }
+}
+
+if (!$found) {
+    $messages['li_error'] = "No LI's Found!";
+}else{
+    $messages['lis_found'] =  implode(',', array_unique($lis_found));
+}
+
+echo json_encode($messages);
+
+clearstatcache();
+exit;
+
+if (isset($_GET['url'])) {
+    $url = urldecode($_GET['url']); // Decode URL-encoded string
+
+
+    // Use basename() function to return the base name of file
+    $file_name = basename($url);
+
+    // Use file_get_contents() function to get the file
+    // from url and use file_put_contents() function to
+    // save the file by using base name
+    if (file_put_contents($file_name, file_get_contents($url))) {
+        echo "File downloaded successfully";
+    } else {
+        echo "File downloading failed.";
+    }
+}
---
+++ static/js/index.js
@@ -0,0 +1,73 @@
+// Also see: https://www.quirksmode.org/dom/inputfile.html
+// document.addEventListener('load', ()=>{
+//   document.getElementById("translate").style.display = "none";
+// });
+
+
+const checkbox = document.getElementById('checkbox');
+
+checkbox.addEventListener('change', () => {
+  document.body.classList.toggle('dark');
+  localStorage.setItem("KEY", "value");
+  // localStorage.getItem("KEY");
+})
+
+// localStorage.setItem("Max", "Keme");
+var inputs = document.querySelectorAll('.file-input')
+
+for (var i = 0, len = inputs.length; i < len; i++) {
+  customInput(inputs[i])
+}
+
+function customInput(el) {
+  const fileInput = el.querySelector('[type="file"]')
+  const label = el.querySelector('[data-js-label]')
+
+  fileInput.onchange =
+    fileInput.onmouseout = function () {
+      if (!fileInput.value) return
+
+      var value = fileInput.value.replace(/^.*[\\\/]/, '')
+      el.className += ' -chosen'
+      label.innerText = value
+    }
+}
+
+const form = document.querySelector("form");
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  document.getElementById("loader").style.display = "block";
+
+  const formData = new FormData(form);
+  const url = window.location.href + "/src/functions.php";
+  axios
+    .post(url, formData, {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    })
+    .then((res) => {
+      console.log(res);
+      document.getElementById("loader").style.display = "none";
+      document.getElementById('content').innerHTML = res.data.text;
+      document.getElementById('flis').innerHTML = res.data.lis_found;
+      document.getElementById("clear").style.display = "block";
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  
+});
+
+const clearBtn = document.getElementById('clear');
+clearBtn.addEventListener('click', () => {
+  document.getElementById('content').innerHTML = '';
+  document.getElementById("clear").style.display = "none";
+});
+
+const textUploadBtn = document.getElementById('textUpload');
+textUploadBtn.addEventListener('click', () => {
+  let text = document.getElementById('textAreaExample').value;
+  document.getElementById('text-content').innerHTML = text;
+  document.getElementById("clear").style.display = "block";
+});
